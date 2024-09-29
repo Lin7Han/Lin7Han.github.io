f324fcdce5ac8c0d9c7273939d759ac3b95f2c4b
Lin7Han <941532182@qq.com>
2024-09-29 09:34:44 +0800

    Duplication

---
+++ assets/js/1df93b7f.d9b8686d.js
@@ -0,0 +1,23 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4583],{8198:(e,t,n)=>{n.r(t),n.d(t,{default:()=>h});var r=n(4586),a=n(781),o=n(1107),i=n(4317);const s={heroBanner:"heroBanner_qdFl",buttons:"buttons_AeoN"};var l=n(195),d=n(4848);const c=(0,l.rU)((e=>{let{prefixCls:t,css:n}=e;return{linearGradientButton:n`
+    &.${t}-btn-primary:not([disabled]):not(.${t}-btn-dangerous) {
+      	border-width: 0;
+
+      	> span {
+      	  	position: relative;
+      	}
+
+      	&::before {
+      	  	content: '';
+      	  	background: linear-gradient(135deg, #6253e1, #04befe);
+      	  	position: absolute;
+      	  	inset: 0;
+      	  	opacity: 1;
+      	  	transition: all 0.3s;
+      	  	border-radius: inherit;
+      	}
+
+      	&:hover::before {
+      	  	opacity: 0;
+      	}
+    }
+  `}}));function h(){const{siteConfig:e}=(0,r.A)();var t,n;const l={position:"absolute",top:"0",left:"0",width:"100%",height:"100%",backgroundImage:'url("img/wallpaper/fantasic'+(t=1,n=3,t=Math.ceil(t),n=Math.floor(n),Math.floor(Math.random()*(n-t+1)+t))+'.jpg")',backgroundSize:"100%",backgroundAttachment:"fixed",backgroundRepeat:"no-repeat"};return(0,d.jsxs)(a.A,{children:[(0,d.jsx)("div",{style:l}),(0,d.jsx)("div",{style:{position:"absolute",top:"0",left:"0",width:"100%",height:"100%",backgroundColor:"rgba(0, 0, 0, 0.75)",backgroundSize:"cover",backgroundAttachment:"fixed",backgroundRepeat:"no-repeat"}}),(0,d.jsx)("div",{style:{height:"150px"}}),(0,d.jsx)("header",{className:s.heroBanner,children:(0,d.jsxs)("div",{className:"container",children:[(0,d.jsx)(o.A,{as:"h1",className:"hero__title",style:{fontFamily:"STHUPO",fontSize:150,background:"linear-gradient(135deg, #78FF8A, #77ECFF, #D86AFF)",WebkitBackgroundClip:"text",color:"transparent"},children:e.title}),(0,d.jsx)("div",{style:{height:"50px"}}),(0,d.jsx)(i.Ay,{className:c().styles.linearGradientButton,type:"primary",size:"large",onClick:()=>{window.location.href="./docs/unreal/intro"},children:"\u865a\u5e7b\u5f15\u64ce\u77e5\u8bc6\u5e93"})]})})]})}}}]);
